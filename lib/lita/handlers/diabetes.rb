6ee8bdea74d046e752f66a49bf476c764685ede7
Chris Baker <chris.baker@inin.com>
2015-02-13 16:37:06 -0500

    Convert basically working

--- lib/lita/handlers/diabetes.rb
+++ lib/lita/handlers/diabetes.rb
@@ -1,20 +1,26 @@
 module Lita
     module Handlers
         class Diabetes < Handler
-            $conversionRatio = 18.0182
+            @@conversionRatio = 18.0182
 
-            route(/(?:^|_)(\d{1,3})(?:$_)/, :convert)
+            route(/(?:^|_)(\d{1,3}|\d{1,2}\.\d+)(?:$|_)/, :convert)
+            #route(/\d*/, :convert)
 
             def convert(response)
-                response.reply('done')
+                input = response.matches[0][0]
+                if input.index('.') == nil then
+                    response.reply(input + ' mg/dL is ' + mgdlToMmol(input) + ' mmol/L')
+                else
+                    response.reply(input + ' mmol/L is ' + mmolToMgdl(input) + ' mg/dL')
+                end
             end
 
             def mgdlToMmol(n)
-                return (n.to_i / $conversionRatio).round(1).to_s
+                return (n.to_i / @@conversionRatio).round(1).to_s
             end
 
             def mmolToMgdl(n)
-                return (n.to_f * $conversionRatio).round.to_s
+                return (n.to_f * @@conversionRatio).round.to_s
             end
 
             def mgdlToDcct(n)
