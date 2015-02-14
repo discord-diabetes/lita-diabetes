4f4e29ee7ebf76117cba9c88b993cf21fc389029
Chris Baker <chris.baker@inin.com>
2015-02-13 19:52:17 -0500

    adding skeleton from `lita handler`

--- lib/lita-diabetes.rb
+++ lib/lita-diabetes.rb
@@ -1,1 +1,12 @@
-require "lita/handlers/diabetes"
+require "lita"
+
+Lita.load_locales Dir[File.expand_path(
+  File.join("..", "..", "locales", "*.yml"), __FILE__
+)]
+
+require "lita/handlers/diabetes_handler"
+
+Lita::Handlers::DiabetesHandler.template_root File.expand_path(
+  File.join("..", "..", "templates"),
+ __FILE__
+)
---
+++ spec/lita/handlers/diabetes_handler_spec.rb
@@ -0,0 +1,4 @@
+require "spec_helper"
+
+describe Lita::Handlers::DiabetesHandler, lita_handler: true do
+end
