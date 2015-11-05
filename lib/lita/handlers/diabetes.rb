3afebfc676ef1bdefcdc36567c9df3cd57e7df8a
Chris Baker <chris.baker@inin.com>
2015-11-05 11:29:04 -0800

    add check for url and do not parse fixes #6

--- lib/lita/handlers/diabetes.rb
+++ lib/lita/handlers/diabetes.rb
@@ -1,112 +1,115 @@
-# vim: set tabstop=2 softtabstop=2 shiftwidth=2 expandtab:
+require "uri"
+
 module Lita
-    module Handlers
-        class Diabetes < Handler
-            @@conversionRatio = 18.0182
+  module Handlers
+    class Diabetes < Handler
+      @@conversionRatio = 18.0182
 
-            route(/(?:^|_)(\d{1,3}|\d{1,2}\.\d+)(?:$|_)/, :convert, command: false, help:{
-                '<number>' => 'Convert glucose between mass/molar concentration units.',
-                '_<number>_' => 'Convert glucose between mass/molar concentration units inline. E.g "I started at _125_ today"'
-            })
+      route(/(?:^|_)(\d{1,3}|\d{1,2}\.\d+)(?:$|_)/, :convert, command: false, help:{
+        '<number>' => 'Convert glucose between mass/molar concentration units.',
+        '_<number>_' => 'Convert glucose between mass/molar concentration units inline. E.g "I started at _125_ today"'
+      })
 
-            route(/estimate a1c(?: from average)?\s+(\d{1,3}|\d{1,2}\.\d+)$/i, :estimateA1c, command: true, help: {
-                'estimate a1c [from average] <glucose level>' => 'Estimates A1C based on average BG level'
-            })
-            route(/estimate average(?: from a1c)?\s+(\d{1,3}|\d{1,2}\.\d+)$/i, :estimateAverageFromA1c, command: true, help: {
-                'estimate average [from a1c] <A1C>' => 'Estimates average blood glucose'
-            })
+      route(/estimate a1c(?: from average)?\s+(\d{1,3}|\d{1,2}\.\d+)$/i, :estimateA1c, command: true, help: {
+        'estimate a1c [from average] <glucose level>' => 'Estimates A1C based on average BG level'
+      })
+      route(/estimate average(?: from a1c)?\s+(\d{1,3}|\d{1,2}\.\d+)$/i, :estimateAverageFromA1c, command: true, help: {
+        'estimate average [from a1c] <A1C>' => 'Estimates average blood glucose'
+      })
 
-            def convert(response)
-                input = response.matches[0][0]
-                Lita.logger.debug('Converting BG for input "' + input + '"')
-                if input.index('.') == nil then
-                    response.reply(input + ' mg/dL is ' + mgdlToMmol(input).to_s + ' mmol/L')
-                else
-                    response.reply(input + ' mmol/L is ' + mmolToMgdl(input).to_s + ' mg/dL')
-                end
-            end
+      def convert(response)
+        if response.message.body.match(URI.regexp(["http", "https"])).nil?
+          input = response.matches[0][0]
+          Lita.logger.debug('Converting BG for input "' + input + '"')
+          if input.index('.') == nil then
+            response.reply(input + ' mg/dL is ' + mgdlToMmol(input).to_s + ' mmol/L')
+          else
+            response.reply(input + ' mmol/L is ' + mmolToMgdl(input).to_s + ' mg/dL')
+          end
+        end
+      end
 
-            def estimateA1c(response)
-                input = response.matches[0][0]
-                Lita.logger.debug('Estimating a1c for input "' + input + '"')
-                mmol = 0
-                mgdl = 0
+      def estimateA1c(response)
+        input = response.matches[0][0]
+        Lita.logger.debug('Estimating a1c for input "' + input + '"')
+        mmol = 0
+        mgdl = 0
 
-                if input.index('.') == nil then
-                    mgdl = input.to_i
-                    mmol = mgdlToMmol(mgdl)
-                else
-                    mmol = input.to_f.round(1)
-                    mgdl = mmolToMgdl(mmol)
-                end
-
-                dcct = mgdlToDcct(mgdl)
-                reply = 'an average of ' + mgdl.to_s + ' mg/dL or '
-                reply = reply + mmol.to_s + ' mmol/L'
-                reply = reply + ' is about '
-                reply = reply + dcct.round(1).to_s + '% (DCCT) or '
-                reply = reply + dcctToIfcc(dcct).round(0).to_s + ' mmol/mol (IFCC)'
-                response.reply(reply)
-            end
-
-            def estimateAverageFromA1c(response)
-                input = response.matches[0][0]
-                Lita.logger.debug('Converting a1c to BG for input "' + input + '"')
-                a1c = input.to_f
-                dcct = 0
-                ifcc = 0
-
-                if input.index('.') == nil then
-                    ifcc = a1c.round(0)
-                    dcct = ifccToDcct(a1c).round(1)
-                else
-                    dcct = a1c.round(1)
-                    ifcc = dcctToIfcc(a1c).round
-                end
-
-                mgdl = dcctToMgdl(dcct)
-
-                reply = 'an A1C of ' + dcct.to_s + '% (DCCT) or '
-                reply = reply + ifcc.to_s + ' mmol/mol (IFCC)'
-                reply = reply + ' is about '
-                reply = reply + mgdl.round.to_s + ' mg/dL or '
-                reply = reply + mgdlToMmol(mgdl).round(1).to_s + ' mmol/L'
-                response.reply(reply)
-            end
-
-            def mgdlToMmol(n)
-                return (n.to_i / @@conversionRatio).round(1)
-            end
-
-            def mmolToMgdl(n)
-                return (n.to_f * @@conversionRatio).round
-            end
-
-            def mgdlToDcct(n)
-                return ((n.to_i + 46.7) / 28.7)
-            end
-
-            def mgdlToIfcc(n)
-                return ((mgdlToDcct(n) - 2.15) * 10.929)
-            end
-
-            def dcctToIfcc(n)
-                return (n.to_f - 2.15) * 10.929
-            end
-
-            def ifccToDcct(n)
-                return (n.to_f / 10.929) + 2.15
-            end
-
-            def dcctToMgdl(n)
-                return (n.to_f * 28.7) - 46.7
-            end
-
-            def ifccToMgdl(n)
-                return dcctToMgdl((n / 10.929) + 2.5)
-            end
+        if input.index('.') == nil then
+          mgdl = input.to_i
+          mmol = mgdlToMmol(mgdl)
+        else
+          mmol = input.to_f.round(1)
+          mgdl = mmolToMgdl(mmol)
         end
 
-        Lita.register_handler(Diabetes)
+        dcct = mgdlToDcct(mgdl)
+        reply = 'an average of ' + mgdl.to_s + ' mg/dL or '
+        reply = reply + mmol.to_s + ' mmol/L'
+        reply = reply + ' is about '
+        reply = reply + dcct.round(1).to_s + '% (DCCT) or '
+        reply = reply + dcctToIfcc(dcct).round(0).to_s + ' mmol/mol (IFCC)'
+        response.reply(reply)
+      end
+
+      def estimateAverageFromA1c(response)
+        input = response.matches[0][0]
+        Lita.logger.debug('Converting a1c to BG for input "' + input + '"')
+        a1c = input.to_f
+        dcct = 0
+        ifcc = 0
+
+        if input.index('.') == nil then
+          ifcc = a1c.round(0)
+          dcct = ifccToDcct(a1c).round(1)
+        else
+          dcct = a1c.round(1)
+          ifcc = dcctToIfcc(a1c).round
+        end
+
+        mgdl = dcctToMgdl(dcct)
+
+        reply = 'an A1C of ' + dcct.to_s + '% (DCCT) or '
+        reply = reply + ifcc.to_s + ' mmol/mol (IFCC)'
+        reply = reply + ' is about '
+        reply = reply + mgdl.round.to_s + ' mg/dL or '
+        reply = reply + mgdlToMmol(mgdl).round(1).to_s + ' mmol/L'
+        response.reply(reply)
+      end
+
+      def mgdlToMmol(n)
+        return (n.to_i / @@conversionRatio).round(1)
+      end
+
+      def mmolToMgdl(n)
+        return (n.to_f * @@conversionRatio).round
+      end
+
+      def mgdlToDcct(n)
+        return ((n.to_i + 46.7) / 28.7)
+      end
+
+      def mgdlToIfcc(n)
+        return ((mgdlToDcct(n) - 2.15) * 10.929)
+      end
+
+      def dcctToIfcc(n)
+        return (n.to_f - 2.15) * 10.929
+      end
+
+      def ifccToDcct(n)
+        return (n.to_f / 10.929) + 2.15
+      end
+
+      def dcctToMgdl(n)
+        return (n.to_f * 28.7) - 46.7
+      end
+
+      def ifccToMgdl(n)
+        return dcctToMgdl((n / 10.929) + 2.5)
+      end
     end
+
+    Lita.register_handler(Diabetes)
+  end
 end
--- spec/lita/handlers/diabetes_spec.rb
+++ spec/lita/handlers/diabetes_spec.rb
@@ -9,11 +9,15 @@ describe Lita::Handlers::Diabetes, lita_handler: true do
     it { is_expected.to route_command "estimate average 5.7" }
     it { is_expected.to route_command "estimate average 45" }
     it "converts mg/dL to mmol/L" do
-        send_message("90")
-        expect(replies.last).to eq("90 mg/dL is 5.0 mmol/L")
+      send_message("90")
+      expect(replies.last).to eq("90 mg/dL is 5.0 mmol/L")
     end
     it "converts mmol/L to mg/dL" do
-        send_message("5.5")
-        expect(replies.last).to eq("5.5 mmol/L is 99 mg/dL")
+      send_message("5.5")
+      expect(replies.last).to eq("5.5 mmol/L is 99 mg/dL")
+    end
+    it "ignores URI in message" do
+      send_message("https://www.reddit.com/r/diabetes/comments/354r77/20yo_diagnosed_with_type_1_today/cr17as0?context=3")
+      expect(replies.last).to be(nil)
     end
 end
